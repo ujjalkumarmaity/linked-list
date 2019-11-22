ae460be1496df943782275fce2fc63a45e1f1f56
ujjal kumar maity <54595503+ujjalkumarmaity@users.noreply.github.com>
2019-11-22 12:04:28 +0530

    Add files via upload

---
+++ single_linked_list_insert_any_position.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<malloc.h>
+struct node
+{
+	int data;
+	struct node *link;
+}*first,*temp,*head,*p;
+
+void insert(int item)
+{
+	temp=(struct node *)malloc(sizeof(struct node));
+	temp->data=item;
+	if(first==0)
+		first=temp;
+	else
+		head->link=temp;
+	temp->link=0;
+	head=temp;
+	
+}
+void insert_any(int item,int pos)
+{
+	p=(struct node *)malloc(sizeof(struct node));
+	p->data=item;
+	int counter=0;
+	temp=first;
+	if(pos==0)
+	{
+		p->link=temp;
+		first=p;	
+		return ;
+	}
+	while(temp!=0)
+	{
+		counter+=1;
+		if(pos==counter)
+		{
+			p->link=temp->link;
+			temp->link=p;
+			return;
+		}
+		temp=temp->link;
+		
+	}
+	printf("insertion is not possible");
+		
+}
+void display()
+{
+	temp=first;
+	while(temp!=0)
+	{
+		printf("%d\n",temp->data);
+		temp=temp->link;
+	}
+}
+int main()
+{
+	insert(10);
+	printf("one\n");
+	insert(11);
+	printf("one\n");
+	insert(12);
+	printf("one\n");
+	insert(13);
+	printf("one\n");
+	insert_any(14,0);
+	display();
+	return 0;
+}
